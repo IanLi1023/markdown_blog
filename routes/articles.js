dd8296e0e863b3c8b19cd05d2a5507e66fb6075a
Ian Li <ian.li1023@gmail.com>
2022-06-13 16:48:52 -0600

    added slugify

--- routes/articles.js
+++ routes/articles.js
@@ -6,9 +6,9 @@ router.get('/new', (req, res) => {
     res.render('articles/new', { article: new Article() })
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:slug', async (req, res) => {
     // res.send(req.params.id)
-    const article = await Article.findById(req.params.id)
+    const article = await Article.findOne({ slug: req.params.slug })
     if (article == null) res.redirect('/')
     res.render('articles/show', { article: article })
 })
@@ -21,7 +21,7 @@ router.post('/', async (req, res) => {
     })
     try {
         article = await article.save()
-        res.redirect(`/articles/${article.id}`)
+        res.redirect(`/articles/${article.slug}`)
     } catch (e) {
         res.render('articles/new', { article: article })
     }
