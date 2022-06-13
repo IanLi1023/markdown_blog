4ad0f3a57e8ebb8a80d59621189067c258483aff
Ian Li <ian.li1023@gmail.com>
2022-06-13 16:05:23 -0600

    render all blog post

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-const express = require ('express');
+const express = require('express');
+const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles');
 const app = express();
 
+mongoose.connect('mongodb://localhost/blog', {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+})
+
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: false }))
 app.use('/articles', articleRouter)
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test Description '
-    }]
-    res.render('articles/index', {articles: articles })
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc' })
+        res.render('articles/index', { articles: articles })
+
+    
 });
+
+app.use('/articles', articleRouter)
 app.listen(5000)
